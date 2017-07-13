93afb786700ac0607acd782ec0d32f726f559dbf
weibin <weibin871128@gmail.com>
2017-07-13 17:28:47 +0800

    Set DEBUG=False for Heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -143,8 +143,10 @@ if os.getcwd()=='/app':
 	#让request.is_secure()承认X-Forwarded-Proto头
 	SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO','https')
 	
-	#支持所有的主机头
-	ALLOWED_HOSTS=['*']
+	#只允许Heroku托管这个项目
+	ALLOWED_HOSTS=['learning-log.herokuapp.com']
+	
+	DEBUG=False
 	
 	#静态资产配置
 	BASE_DIR=os.path.dirname(os.path.abspath(__file__))
